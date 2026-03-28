88b9cc90448e0e07c97e158bf66446c341d0a4ca
swyamshukla <swyamshukla0104@gmail.com>
2026-03-28 09:31:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Paths from root with a specified sum/paths-from-root-with-a-specified-sum.java
@@ -0,0 +1,41 @@
+// User function Template for Java
+
+/*Tree Node
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    
+    static void paths(Node root, int sum, int curr, ArrayList<ArrayList<Integer>> result,ArrayList<Integer> temp ){
+        if(root==null) return;
+        // ArrayList<Integer> temp = new ArrayList<>();
+        curr+=root.data;
+        temp.add(root.data);
+        if(curr==sum) result.add(new ArrayList<>(temp));
+        // System.out.println(temp);
+        // System.out.println(curr);
+        
+        paths(root.left,sum,curr,result,temp);
+        paths(root.right,sum,curr,result,temp);
+        temp.remove(temp.size()-1);
+        curr-=root.data;
+    }
+    
+    public static ArrayList<ArrayList<Integer>> printPaths(Node root, int sum) {
+        // code here
+        
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+            ArrayList<Integer> temp = new ArrayList<>();
+        paths(root,sum,0,result,temp);
+        return result;
+    }
+}
