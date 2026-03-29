95d488a7a17de64167185acd12cfa7a976375066
swyamshukla <swyamshukla0104@gmail.com>
2026-03-29 21:40:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.java
@@ -0,0 +1,91 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int d) {
+        data = d;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    
+    static void addLeftTree(Node root,ArrayList<Integer> result){
+        if(root==null) return;
+        if(root.left ==null && root.right==null){
+            // System.out.println("hello"+root.data);
+            return;
+        }
+           result.add(root.data);
+
+
+        if(root.left!=null){
+            addLeftTree(root.left,result);
+        }
+        else{
+            addLeftTree(root.right,result);
+        }
+    }
+    
+    static void addLeaf(Node root,ArrayList<Integer> result ){
+        if(root==null) return;
+        if(root.left==null && root.right==null){
+            result.add(root.data);
+        }
+        addLeaf(root.left,result);
+        addLeaf(root.right,result);
+    }
+    
+    static void addRightTree(Node root,ArrayList<Integer> hold){
+        if(root ==null) return;
+         if(root.left ==null && root.right==null){
+            // System.out.println("hello"+root.data);
+            return;
+        }
+        hold.add(root.data);
+        
+        if(root.right!=null){
+            addRightTree(root.right,hold);
+        }
+        else{
+            addRightTree(root.left,hold);
+        }
+        
+    }
+    
+    
+        
+    
+    ArrayList<Integer> boundaryTraversal(Node root) {
+        // code here
+        ArrayList<Integer> result = new ArrayList<>();
+        if(root.left==null && root.right==null){
+            result.add(root.data);
+            return result;
+        }
+        // Stack<Node> stack = new Stack<>();
+        result.add(root.data);
+        addLeftTree(root.left,result);
+        addLeaf(root,result);
+        // System.out.println(result);
+        
+        
+        
+        ArrayList<Integer> hold = new ArrayList<>();
+        
+        addRightTree(root.right,hold);
+
+        for(int i=hold.size()-1;i>=0;i--){
+            result.add(hold.get(i));
+        }
+        
+        
+        
+        return result;
+        
+
+    }
+}
+
