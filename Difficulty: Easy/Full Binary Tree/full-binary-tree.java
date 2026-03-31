c6983610f4b5727f9a4e4a6a187389bff3174601
swyamshukla <swyamshukla0104@gmail.com>
2026-03-31 23:47:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Full Binary Tree/full-binary-tree.java
+++ Difficulty: Easy/Full Binary Tree/full-binary-tree.java
@@ -14,16 +14,11 @@ class Node
 */
 class GfG {
     // Return True if the given Binary Tree is a Full Binary Tree. Else return False
-        static boolean check(Node node){
-            if(node==null) return true;
-            if(node.left==null && node.right==null) return true;
-            if(node.left==null || node.right==null) return false;
-            
-            return check(node.left) && check(node.right);
-        }
-    
     boolean isFullTree(Node node) {
         // add code here.
-        return check(node);
+        if(node==null) return true;
+        if(node.left==null && node.right==null) return true;
+        if(node.left==null || node.right==null) return false;
+        return isFullTree(node.left) && isFullTree(node.right);
     }
 }
