499281da4d5f0dd1b1cec1def535bf71ae9996cc
swyamshukla <swyamshukla0104@gmail.com>
2026-04-01 23:12:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/ZigZag Tree Traversal/zig-zag-tree-traversal.java
+++ Difficulty: Medium/ZigZag Tree Traversal/zig-zag-tree-traversal.java
@@ -11,31 +11,36 @@ class Node {
 */
 
 class Solution {
-    ArrayList<Integer> zigZagTraversal(Node root) {
+  ArrayList<Integer> zigZagTraversal(Node root) {
         // code here
         
         ArrayList<Integer> result= new ArrayList<>();
-        Queue<Node> q = new LinkedList<>();
+        Deque<Node> q = new ArrayDeque<>();
         
         q.add(root);
         int level=0;
+        
         while(!q.isEmpty()){
             int size= q.size();
             ArrayList<Integer> hold = new ArrayList<>();
             for(int i=0;i<size;i++){
                 
-                Node temp =q.remove();
-                if(temp.left!=null) q.add(temp.left);
-                if(temp.right!=null) q.add(temp.right);
-                hold.add(temp.data);
+                if(level%2!=0){
+                    Node temp =q.removeLast();
+                    if(temp.right!=null) q.addFirst(temp.right);
+                    if(temp.left!=null) q.addFirst(temp.left);
+                    hold.add(temp.data);                    
+                }
+                else{
+                    Node temp =q.removeFirst();
+                    if(temp.left!=null) q.addLast(temp.left);
+                    if(temp.right!=null) q.addLast(temp.right);  
+                    hold.add(temp.data);
+                }
+
+
             }
-            if(level%2!=0) {
-                Collections.reverse(hold);
-                result.addAll(hold);
-            }
-            else{
-                result.addAll(hold);
-            }
+            result.addAll(hold);
             level++;
         }
         
