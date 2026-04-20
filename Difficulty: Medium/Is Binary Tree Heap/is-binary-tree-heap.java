6c5804bf573e7a8552ac5fa2cf2c7db79cd80d84
swyamshukla <swyamshukla0104@gmail.com>
2026-04-20 17:04:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Is Binary Tree Heap/is-binary-tree-heap.java
+++ Difficulty: Medium/Is Binary Tree Heap/is-binary-tree-heap.java
@@ -12,33 +12,28 @@ class Node {
 class Solution {
     
     static int count(Node tree){
-        
         if(tree==null) return 0;
         return 1+count(tree.left)+count(tree.right);
-        
+    }
+    // chcek for cbt-->
+    
+    static boolean checkCBT(Node tree,int idx,int size){
+        if(tree==null) return true;
+        if(idx>=size) return false;
+        return checkCBT(tree.left,(2*idx)+1,size) && checkCBT(tree.right,(2*idx)+2,size);
     }
     
-    static boolean check(Node root){
-        if(root == null ) return true;
-        if(root.left!=null && root.left.data>root.data) return false;
-        if(root.right!=null && root.right.data>root.data) return false;
-        return check(root.left) && check(root.right);
-        
-    }
-    
-    static boolean checkCBT(Node tree,int idx,int count){
-        if(tree == null) return true;
-        if(idx>=count) return false;
-        
-        return checkCBT(tree.left,2*idx+1,count) && checkCBT(tree.right,2*idx+2,count);
+    static boolean checkMaxHeap(Node tree){
+        if(tree==null) return true;
+        if(tree.left!=null && tree.left.data>tree.data) return false;
+        if(tree.right!=null && tree.right.data>tree.data) return false;
+        return checkMaxHeap(tree.left) && checkMaxHeap(tree.right);
     }
     
     public boolean isHeap(Node tree) {
         // code here
-        int count =count(tree);
-        
-        return check(tree) && checkCBT(tree,0,count);
-        
+        int size=count(tree);
+        return checkCBT(tree,0,size) && checkMaxHeap(tree);
         
     }
 }
